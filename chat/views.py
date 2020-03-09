966aa330f9780f4e8edb9204317a5047fe9ca89a
skru <skruproxy@gmail.com>
2020-03-09 10:16:57 +0000

    installing vue on prod

--- chat/views.py
+++ chat/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.template import RequestContext
 from django.conf import settings
 
-def index(request):
-    return render(request, 'chat/index.html', {})
+# def index(request):
+#     return render(request, 'chat/index.html', {})
 
-def room(request, room_name):
-    return render(request, 'chat/room.html', {
-        'room_name': room_name,
-        'unsecure_socket': settings.DEBUG
-    })
+# def room(request, room_name):
+#     return render(request, 'chat/room.html', {
+#         'room_name': room_name,
+#         'unsecure_socket': settings.DEBUG
+#     })
